3092b952e79e7562713d6c04c8c62cfe8a707036
Jan Holub <Jan-Holub@gmx.net>
2016-06-24 15:53:53 +0200

    Queued songs are now downloaded on startup, if the 'cached' flag is set

--- server/startup.js
+++ server/startup.js
@@ -1,5 +1,7 @@
+import downloader from 'youtube-dl';
 import mpv from 'node-mpv';
 import fs  from 'fs';
+
 
 // global variable to access mpv
 mpv_player = null;
@@ -14,12 +16,27 @@ Meteor.startup(function() {
         audio_only: true
     });
 
-    // initialize youtube-dl
-   // youtube_dl = Meteor.require('youtube-dl');
-
     // create the folder to store the cached songs
     if(!fs.existsSync('songs')){
         fs.mkdirSync('songs');
+    }
+
+    // download every song if cached mode is activated
+    if(cached){
+        // arguments to select the format for youtube-dl
+        var args = ['--format=251/171/140/250/249/best'];
+
+        // download each song in the playlist
+        Playlist.find().forEach(function(queuedSong){
+            // download the song
+            var song = downloader(queuedSong.url, args);
+            // write it to the HDD
+            song.pipe(fs.createWriteStream(`songs/${queuedSong.title}.mp3`));
+
+            console.log(`Handling title "${queuedSong.title}`);
+        });
+
+
     }
 
 });
