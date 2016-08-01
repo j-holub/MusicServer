2f6bcf2e956693aeeb5c64efa84499cf2d07de1d
Jan Holub <Jan-Holub@gmx.net>
2016-08-01 21:30:09 +0200

    Styled the Enqueue Bar and increased the usability by adding Keyboard support

--- client/enqueue/enqueue.js
+++ client/enqueue/enqueue.js
@@ -1,22 +1,40 @@
 
+// This function is called when the button is clicked or the return key is pressed
+var addSong = function() {
+	// get the url
+	var url = $('#enqueueInput').val();
+	if(!(url === "")){	
+		Meteor.call('enqueue', url, function(error, result) {
+			if(!error){
+				// set the input field to empty
+				$('#enqueueInput').val("");
+				// leave the focus
+				$('#enqueueInput').blur();
+			}
+			// if there was an error handle it
+			else{
+				if(error.error == "url_invalid"){
+					alert("Not a valid URL");
+				}				
+			}
+		});
+		
+	}
+}
+
+
 Template.enqueue.events({
 	'click #enqueueButton': function (event) {
-		// get the url
-		var url = $('#enqueueInput').val();
-		if(!(url === "")){	
-			Meteor.call('enqueue', url, function(error, result) {
-				if(!error){
-					// set the input field to empty
-					$('#enqueueInput').val("");
-				}
-				// if there was an error handle it
-				else{
-					if(error.error == "url_invalid"){
-						alert("Not a valid URL");
-					}				
-				}
-			});
-			
+		addSong();
+	},
+	'keypress #enqueueInput': function (event) {
+		// Enter / Return
+		if(event.charCode == 13){
+			addSong();
+		}
+		// Escape
+		else if(event.charCode == 27){
+			$('#enqueueInput').blur();
 		}
 	}
 });
