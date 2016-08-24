558e5c35934ee5039a7c64030b0b1599749e2775
Jan Holub <Jan-Holub@gmx.net>
2016-08-25 00:20:13 +0200

    Implemented a function to compute the dominant color of a thumbnail on the server

--- server/information.js
+++ server/information.js
@@ -9,5 +9,30 @@ Meteor.methods({
     },
     getCurrentSong: function() {
     	return Playlist.findOne({'position': 0});
+    },
+    // returns the dominant color of a thumbnail image to a song
+    dominantColors: function(songId) {
+        var song = Playlist.findOne(songId);
+        var thumbnailFile = song.thumbnail.getFileRecord({'store': 'Thumbnail'});
+
+        var colorHex;
+
+        // compute the dominant color
+        var response = Async.runSync(function(done) {
+             gm(thumbnailFile.createReadStream())
+            .resize(250, 250)
+            .fuzz('10%')
+            .trim() // remove black borders
+            .colors(2) // get the two most dominant colors
+            .toBuffer('RGB', function (error, buffer) {
+                // the dominant color
+                colorHex = buffer.toString('hex', 3, 6);
+                console.log("#" + colorHex);
+                done(null, colorHex);
+            });
+        })
+
+        // the dominant color
+        return response.result;
     }
 });
